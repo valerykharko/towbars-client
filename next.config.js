adfa84e7940918e471f11a71ca8d393534c9470e
valerykharko <x-valery@mail.ru>
2022-11-20 19:05:35 +0300

    client: create proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
+  },
   env: {
     API_URL: process.env.API_URL,
   },
