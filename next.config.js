a6e8004f396d99f41e909cc42cdc9a31b90073c0
valerykharko <x-valery@mail.ru>
2022-11-20 22:33:48 +0300

    client: manage env's files; Delivery responsive design

--- next.config.js
+++ next.config.js
@@ -7,16 +7,12 @@ module.exports = {
         source: "/api/:path*",
         destination: "http://localhost:5000/api/:path*",
       },
-      {
-        source: "/",
-        destination: "http://localhost:5000/",
-      },
     ];
   },
   env: {
     API_URL: process.env.API_URL,
   },
   images: {
-    domains: [process.env.NEXT_PUBLIC_SERVER_HOST, process.env.NEXT_PUBLIC_SERVER_HOST_ORIGINAL],
+    domains: [process.env.NEXT_PUBLIC_SERVER_HOST],
   },
 };
